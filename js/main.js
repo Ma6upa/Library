3251c5cf9deaa86bf84ec9a237a3e417c43a5e59
Yudintsev Valeriy <yuvalerka@mail.ru>
2021-07-07 13:43:57 +0400

    Added 'my books' section

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ let upload = document.getElementById('upload');
 let writeForm = document.forms.writeForm;
 let uploadForm = document.forms.uploadForm;
 
-let library = [];
+var data = localStorage.getItem('library');
+var library = data ? JSON.parse(data) : [];
 
 writeForm.style = 'display: none';
 uploadForm.style = 'display: none';
@@ -25,9 +26,11 @@ document.querySelector('#writeButton').addEventListener('click', function writeB
         title: title,
         text: text,
         id: Date.now(),
+        wasRead: false
     }
     library.push(book);
-    localStorage.setItem('xdd', JSON.stringify(library));
+    localStorage.setItem('library', JSON.stringify(library));
+    location.reload()
 });
 
 document.querySelector('#uploadButton').addEventListener('click', function uploadBook () {
@@ -39,10 +42,155 @@ document.querySelector('#uploadButton').addEventListener('click', function uploa
             let book = {
                 title: formData.get('login'),
                 text: JSON.parse(this.response).text,
-                id: Date.now()
+                id: Date.now(),
+                wasRead: false
             }
             library.push(book);
             localStorage.setItem('library', JSON.stringify(library));
+            location.reload()
     }
     xhr.send(formData);
 });
+
+function sort() {
+    let unsorted = localStorage['library'];
+    localStorage['library'] != undefined ? unsorted = JSON.parse(localStorage['library']) : console.log('пустой LS');
+    let read = [];
+    let unread = [];
+    for(let i = 0; i < unsorted.length; i++){
+        if(unsorted[i].wasRead == true){
+            read.push(unsorted[i]);
+        } else {
+            unread.push(unsorted[i]);
+        }
+    }
+    read.sort(function (a, b) {
+        return b.id - a.id;
+    });
+    unread.sort(function (a, b) {
+        return b.id - a.id;
+    });
+    let sorted = read.concat(unread);
+    localStorage.setItem('library', JSON.stringify(sorted));
+}
+
+function read(id){
+    let books = localStorage['library'];
+    localStorage['library'] != undefined ? books = JSON.parse(localStorage['library']) : console.log('пустой LS')
+    let book = books.find(book => book.id == id);
+    let readSpace = document.createElement('div');
+    let oldSpace = document.getElementsByClassName('read__space');
+    document.querySelector('.read').replaceChild(readSpace, oldSpace[0]);
+    readSpace.classList.add('read__space');
+    readSpace.innerHTML = book.text;
+}
+
+function deleteBook(id){
+    let books = localStorage['library'];
+    localStorage['library'] != undefined ? books = JSON.parse(localStorage['library']) : console.log('пустой LS');
+    let book = books.find(book => book.id === id);
+    let number =  books.indexOf(book);
+    books.splice(number, 1);
+    localStorage.setItem('library', JSON.stringify(books));
+    bookList();
+}
+
+function changeStatus(id){
+    let books = localStorage['library'];
+    localStorage['library'] != undefined ? books = JSON.parse(localStorage['library']) : console.log('пустой LS');
+    let book = books.find(book => book.id === id);
+    let number =  books.indexOf(book);
+    books[number].wasRead = !books[number].wasRead;
+    localStorage.setItem('library', JSON.stringify(books));
+    bookList();
+}
+
+function edit(id){
+    document.querySelector('.read__space').innerHTML='';
+    let books = localStorage['library'];
+    localStorage['library'] != undefined ? books = JSON.parse(localStorage['library']) : console.log('пустой LS ')
+    let book = books.find(book => book.id === id);
+    let newForm = document.createElement('form');
+    newForm.style.display = 'flex';
+    newForm.style.flexDirection = 'column';
+    let newTitle = document.createElement('input');
+    newTitle.setAttribute('value', `${books[books.indexOf(book)].title}`);
+    let newText = document.createElement('textarea');
+    newText.style.width = '70%';
+    newText.style.height = '70%';
+    newText.style.wordWrap = 'break-word';
+    newText.innerHTML = `${books[books.indexOf(book)].text}`;
+    let newButton = document.createElement('input');
+    newButton.setAttribute('type', 'button');
+    newButton.setAttribute('value', 'Сохранить книгу');
+    newButton.addEventListener('click', function(){
+        books[books.indexOf(book)].title = newTitle.value;
+        books[books.indexOf(book)].text = newText.value;
+        document.querySelector('.read__space').innerHTML='';
+        localStorage.setItem('library', JSON.stringify(books));
+        bookList()
+    });
+    document.querySelector('.read__space').appendChild(newForm);
+    newForm.appendChild(newTitle);
+    newForm.appendChild(newText);
+    newForm.appendChild(newButton);
+}
+
+function bookList() {
+    sort()
+    let booksList = document.querySelector('.books__list');
+    booksList.innerHTML = '';
+    let books = localStorage['library'];
+    localStorage['library'] != undefined ? books = JSON.parse(localStorage['library']) : console.log('пустой LS');
+
+    if(localStorage['library'] != undefined){
+        let booksArrCopy = books
+
+        for(let i = 0; i < booksArrCopy.length ; i++) {
+            let id = booksArrCopy[i].id.toString();
+            let book = document.createElement('div');
+
+            booksList.appendChild(book);
+            if (booksArrCopy[i].wasRead == false){
+                book.classList.add('books__item');
+            } else {
+                book.classList.add('books__item-wasRead')
+            }
+
+            let bookTitle = document.createElement('div');
+            book.appendChild(bookTitle);
+            bookTitle.classList.add('books__item-title');
+            bookTitle.innerHTML = booksArrCopy[i].title;
+    
+            let readButton = document.createElement('input');
+            book.appendChild(readButton)
+            readButton.classList.add('books__item-button');
+            readButton.setAttribute('type', 'button');
+            readButton.setAttribute('value', 'Читать книгу');
+            readButton.setAttribute('onClick', `read(${id})`);
+    
+            let deleteButton = document.createElement('input');
+            book.appendChild(deleteButton)
+            deleteButton.classList.add('book_card-button');
+            deleteButton.setAttribute('type', 'button');
+            deleteButton.setAttribute('value', 'Удалить книгу');
+            deleteButton.setAttribute('onClick', `deleteBook(${id})`);
+    
+            let changeStatusButton = document.createElement('input');
+            book.appendChild(changeStatusButton)
+            changeStatusButton.classList.add('books__item-button');
+            changeStatusButton.setAttribute('type', 'button');
+            changeStatusButton.setAttribute('value', 'Статус');
+            changeStatusButton.setAttribute('onClick', `changeStatus(${id})`);
+    
+            let editButton = document.createElement('input');
+            book.appendChild(editButton)
+            editButton.classList.add('books__item-button');
+            editButton.setAttribute('type', 'button');
+            editButton.setAttribute('value', 'Редактировать книгу');
+            editButton.setAttribute('onClick', `edit(${id})`);
+        }
+    }
+}
+
+window.onload = bookList
